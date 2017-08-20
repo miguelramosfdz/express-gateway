c3cc1d80b4688ef1f220c4cc6bc0f9b14d7d67c4
DrMegavolt <sergeykuc@gmail.com>
2017-08-20 20:11:11 +0300

    engine to load plugins

--- examples/eg-example-plugin/index.js
+++ examples/eg-example-plugin/index.js
@@ -4,6 +4,13 @@ module.exports = {
     pluginManifest.policies.push(require('./policies/example-policy'));
     pluginManifest.routes.push(require('./routes/hello-eg'));
     pluginManifest.adminRoutes.push(require('./admin-routes/hello-admin'));
+
+    pluginManifest.eventBus.on('hot-reload', function ({type, newConfig}) {
+      console.log('hot-reload', type, newConfig);
+    });
+    pluginManifest.eventBus.on('ready', function ({httpServer, httpsServer, adminServer}) {
+      console.log('ready');
+    });
   }
 
 };
