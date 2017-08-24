ebf27093275f29cfc4a42ab445da05fbdbb626d6
DrMegavolt <sergeykuc@gmail.com>
2017-08-24 17:15:02 +0300

    rm plugin example

--- examples/eg-example-plugin/conditions/url-match.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  name: 'url-match',
-  handler: function (req, actionConfig) {
-    return (actionConfig.expected === req.url);
-  }
-};
--- examples/eg-example-plugin/policies/example-policy.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'policy-from-example-plugin',
-  policy: (actionParams) => {
-    return (req, res) => {
-      // eslint-disable-next-line no-console
-      console.log('executing policy-from-example-plugin with params', actionParams);
-    };
-  }
-};
