3e835053f7642ed03112a90f3ddf298482f7d653
DrMegavolt <sergeykuc@gmail.com>
2017-08-26 22:05:41 +0300

    new tests

--- test/plugins/admin-ext.test.js
+++ test/plugins/admin-ext.test.js
@@ -1,11 +1,36 @@
 const assert = require('assert');
 const admin = require('../../lib/rest');
-
+const request = require('supertest');
 describe('admin with plugins', () => {
-  before('fires up a new gateway instance with valid config', () => {
-    admin({adminExtensions: [function (adminExpressInstance) {
-      assert.ok(adminExpressInstance);
-      adminExpressInstance.all('/*', (req, res) => res.json({enabled: true}));
-    }]});
+  let adminSrv;
+  before('fires up a new admin instance', function () {
+    return admin({
+      plugins: {
+        adminExtensions: [function (adminExpressInstance) {
+          adminExpressInstance.all('/test', (req, res) => res.json({enabled: true}));
+        }]},
+      config: {
+        gatewayConfig: {
+          admin: {
+            port: 0
+          }
+        }
+      }
+    }).then(srv => {
+      adminSrv = srv;
+      return srv;
+    });
+  });
+
+  it('should add custom route', () => {
+    return request(adminSrv)
+      .get('/test')
+      .then(res => {
+        assert.ok(res.body.enabled);
+      });
+  });
+
+  after('close admin srv', () => {
+    adminSrv.close();
   });
 });
